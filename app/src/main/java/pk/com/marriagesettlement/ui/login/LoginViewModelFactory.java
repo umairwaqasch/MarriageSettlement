2ad867e1277acad31d87dac6b6125722a964521c
Waqas CH <umairwaqasch@gmail.com>
2020-02-28 09:10:54 +0500

    Initial commit

---
+++ app/src/main/java/pk/com/marriagesettlement/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package pk.com.marriagesettlement.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import pk.com.marriagesettlement.data.LoginDataSource;
+import pk.com.marriagesettlement.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
